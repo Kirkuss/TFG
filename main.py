ff9c9d7ed034d4802f042bf23f4018268afb9abe
Enrique Valverde Soriano <Enrique.Valverde1@alu.uclm.es>
2021-11-03 18:24:17 +0100

    Fallo tontisimo corregido

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import cv2
 import numpy as np
 
 img = cv2.imread("Resources/foto1.jpg")
-kernel = np.ones((5,5), np.uint8)
 
 class face:
     def __init__(self, x, y, w, h):
@@ -26,37 +25,8 @@ class face:
         else:
             return False
 
-def stackImages(scale,imgArray):
-    rows = len(imgArray)
-    cols = len(imgArray[0])
-    rowsAvailable = isinstance(imgArray[0], list)
-    width = imgArray[0][0].shape[1]
-    height = imgArray[0][0].shape[0]
-    if rowsAvailable:
-        for x in range ( 0, rows):
-            for y in range(0, cols):
-                if imgArray[x][y].shape[:2] == imgArray[0][0].shape [:2]:
-                    imgArray[x][y] = cv2.resize(imgArray[x][y], (0, 0), None, scale, scale)
-                else:
-                    imgArray[x][y] = cv2.resize(imgArray[x][y], (imgArray[0][0].shape[1], imgArray[0][0].shape[0]), None, scale, scale)
-                if len(imgArray[x][y].shape) == 2: imgArray[x][y]= cv2.cvtColor( imgArray[x][y], cv2.COLOR_GRAY2BGR)
-        imageBlank = np.zeros((height, width, 3), np.uint8)
-        hor = [imageBlank]*rows
-        hor_con = [imageBlank]*rows
-        for x in range(0, rows):
-            hor[x] = np.hstack(imgArray[x])
-        ver = np.vstack(hor)
-    else:
-        for x in range(0, rows):
-            if imgArray[x].shape[:2] == imgArray[0].shape[:2]:
-                imgArray[x] = cv2.resize(imgArray[x], (0, 0), None, scale, scale)
-            else:
-                imgArray[x] = cv2.resize(imgArray[x], (imgArray[0].shape[1], imgArray[0].shape[0]), None,scale, scale)
-            if len(imgArray[x].shape) == 2: imgArray[x] = cv2.cvtColor(imgArray[x], cv2.COLOR_GRAY2BGR)
-        hor= np.hstack(imgArray)
-        ver = hor
-    return ver
-
+    def toString(self):
+        print(str(self.x) + " " + str(self.y) + " " + str(self.w) + " " + str(self.h) + " prueba")
 
 cap = cv2.VideoCapture("Resources/video3.mp4")
 #cap = cv2.VideoCapture(0)
@@ -69,7 +39,7 @@ window = "Salida"
 cv2.namedWindow(window)
 cv2.moveWindow(window, 40,30)
 
-faces = []
+f_Found = []
 images = []
 
 while True:
@@ -77,30 +47,23 @@ while True:
     #print(frame.size())
     frame = cv2.resize(frame, (540, 380), fx = 0, fy = 0, interpolation = cv2.INTER_CUBIC)
     GrayFrame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    faces = faceCascade.detectMultiScale(GrayFrame,1.1,4)
+    faces = faceCascade.detectMultiScale(GrayFrame,1.1,4) #detector de caras + confianza
 
     for (x, y, w, h) in faces:
         #count += 1
-        f_found = face(x, y, w, h)
-        np.append(faces, f_found)
+        faceFound = face(x, y, w, h)
+        #faceFound.toString()
+        f_Found.append(faceFound)
 
-        for i in range(len(faces)):
-            print(faces[i])
-            if f_found.equal(1.1, faces[i][1], faces[i][2]) and not f_found.found:
-                np.delete(faces, i)
-                f_found.found = True
-                np.append(faces, f_found)
-                #imgCropped = frame[y:y + h, x:x + w]
-                #imgCropped = cv2.resize(imgCropped, (320, 240))
-            else:
-                pass
+        for i in range(len(f_Found)):
+            print(f_Found.pop(i).toString())
 
         cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
         imgCropped = frame[y:y + h, x:x + w]
         imgCropped = cv2.resize(imgCropped, (320,240))
         #np.append(images, imgCropped)
         #imgStack = stackImages(0.5, images)
-        cv2.imshow("Gepeto" + str(f_found), imgCropped)
+        cv2.imshow("Gepeto", imgCropped)
 
     cv2.imshow(window, frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
