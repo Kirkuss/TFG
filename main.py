d363ac02dd969295eff22f16f9ef97a35da53f36
Enrique Valverde Soriano <Enrique.Valverde1@alu.uclm.es>
2021-11-03 20:07:28 +0100

    Problemas de rendimiento pero avanzamos
    El tiempo de procesamiento se va a niveles estratosfericos (hay un error en el control que aisla las caras)

--- main.py
+++ main.py
@@ -1,7 +1,33 @@
 import cv2
 import numpy as np
+import matplotlib.pyplot as plt
 
+show = True
+detailed = True
+source = "VIDEO"
+path = "video3.mp4"
+
+faceCascade = cv2.CascadeClassifier("Resources/haarcascade_frontalface_default.xml")
 img = cv2.imread("Resources/foto1.jpg")
+window = "Salida"
+
+cv2.namedWindow(window)
+cv2.moveWindow(window, 40,30)
+
+if source=="VIDEO":
+    cap = cv2.VideoCapture("Resources/" + path)
+elif source == "CAM":
+    cap = cv2.VideoCapture(0)
+
+cap.set(3,640)
+cap.set(4,480)
+
+f_Found = []
+founds = 0
+iterations = -1
+
+list_x = []
+list_y = []
 
 class face:
     def __init__(self, x, y, w, h):
@@ -9,13 +35,20 @@ class face:
         self.y = y
         self.w = w
         self.h = h
-        self.found = False
+        # self.visited = False ?
+
+    #Metodo equal:
+    # comprueba que un punto dado esta en un area si coincide
+    # con el area del punto de la cara anterior
+    # la cara es la misma y actualiza el estado
 
     def equal(self, conf, x, y):
-        #x1, y1 = int(self.x * conf),int(self.y * conf)
-        #x2, y2 = int(self.x - (self.x * (1 - conf))),int(self.y - (self.y * (1 - conf)))
+        ix, iy = conf * w, conf * h
+        x1, y1 = int(self.x + ix),int(self.y + iy)
+        x2, y2 = int(self.x - ix),int(self.y - iy)
 
-        x1, x2, y1, y2 = 165, 190, 125, 155
+        if x2 < 0: x2 = 0
+        elif y2 < 0: y2 = 0
 
         rx = range(x2, x1 + 1)
         ry = range(y2, y1 + 1)
@@ -28,46 +61,58 @@ class face:
     def toString(self):
         print(str(self.x) + " " + str(self.y) + " " + str(self.w) + " " + str(self.h) + " prueba")
 
-cap = cv2.VideoCapture("Resources/video3.mp4")
-#cap = cv2.VideoCapture(0)
-cap.set(3,640)
-cap.set(4,480)
-
-faceCascade = cv2.CascadeClassifier("Resources/haarcascade_frontalface_default.xml")
-
-window = "Salida"
-cv2.namedWindow(window)
-cv2.moveWindow(window, 40,30)
-
-f_Found = []
-images = []
-
 while True:
+    iterations += 1
     ret, frame = cap.read()
-    #print(frame.size())
     frame = cv2.resize(frame, (540, 380), fx = 0, fy = 0, interpolation = cv2.INTER_CUBIC)
     GrayFrame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    faces = faceCascade.detectMultiScale(GrayFrame,1.1,4) #detector de caras + confianza
+    faces = faceCascade.detectMultiScale(GrayFrame,1.1,4)
+    list_y.append(founds)
+    list_x.append(iterations)
 
     for (x, y, w, h) in faces:
-        #count += 1
         faceFound = face(x, y, w, h)
-        #faceFound.toString()
-        f_Found.append(faceFound)
 
-        for i in range(len(f_Found)):
-            print(f_Found.pop(i).toString())
+        if len(f_Found) == 0:
+            f_Found.append(faceFound)
+            print("Added First")
+            founds += 1
+        else:
+            for obj in f_Found:
+                #print("hello")
+                if obj.equal(0.5, x, y):
+                    #print("Coincidence found")
+                    f_Found.remove(obj)
+                    f_Found.append(faceFound)
+                else:
+                    f_Found.append(faceFound)
+                    founds += 1
+                    #print("Added new")
 
-        cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
-        imgCropped = frame[y:y + h, x:x + w]
-        imgCropped = cv2.resize(imgCropped, (320,240))
-        #np.append(images, imgCropped)
-        #imgStack = stackImages(0.5, images)
-        cv2.imshow("Gepeto", imgCropped)
+        if detailed:
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
+            imgCropped = frame[y:y + h, x:x + w]
+            imgCropped = cv2.resize(imgCropped, (320,240))
+        else: pass
 
-    cv2.imshow(window, frame)
+        if show:
+            cv2.imshow("Gepeto", imgCropped)
+            cv2.imshow(window, frame)
+        else: pass
+
+        print("Found faces: " + str(founds))
+
+
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
+
+print("x: " + str(len(list_x)))
+print("y: " + str(len(list_y)))
+plt.plot(list_x,list_y)
+plt.xlabel("iterations")
+plt.ylabel("Found faces")
+plt.title("Rendimiento - aislamiento")
+plt.show()
 
 """
 for (x,y,w,h) in faces:
